21fc5a5366d0ac428ad4d1e1ecdb4350366541c7
matik103 <hookarte@gmail.com>
2025-03-30 03:32:22 +0800

    fix: remove duplicate close button from new client modal

--- src/components/client/NewClientModal.tsx
+++ src/components/client/NewClientModal.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -125,9 +124,6 @@ export function NewClientModal({ isOpen, onClose }: NewClientModalProps) {
         <DialogHeader>
           <DialogTitle className="flex justify-between items-center">
             <span>Create New Client</span>
-            <Button variant="ghost" size="icon" onClick={onClose}>
-              <X className="h-4 w-4" />
-            </Button>
           </DialogTitle>
         </DialogHeader>
         
